b71502f94cdd796bf49ac6678642147b30083f46
Jordan Barnes <rjordanbarnes@gmail.com>
2017-10-07 22:55:27 -0700

    Added CreateUsers SQL script

---
+++ sql setup/CreateUsers.sql
@@ -0,0 +1,10 @@
+INSERT INTO Users (Username)
+VALUES ('Jordan'),
+       ('Keysi'),
+       ('Joshua'),
+       ('Timmy'),
+       ('Rini'),
+       ('Salem')
+
+SELECT * FROM Users
+SELECT * FROM Rooms
