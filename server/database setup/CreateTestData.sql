053c82f4966c8e2baf327bc65e37028119dd4b89
Jordan Barnes <rjordanbarnes@gmail.com>
2017-10-29 19:45:39 -0700

    Added IsPlayer column to Users table

--- server/database setup/CreateTestData.sql
+++ server/database setup/CreateTestData.sql
@@ -6,13 +6,13 @@ VALUES ('The Newest Room', NULL, DEFAULT, 1),
        ('Middle Room', NULL, '2017-09-21 16:45:37.200', 1),
        ('Inactive Room', NULL, '2017-09-22 12:45:37.200', 0)
 
-INSERT INTO Users (Username, CurrentRoom, IsHost)
-VALUES ('Jordan', (SELECT RoomID FROM Rooms WHERE RoomName='The Newest Room'), 0),
-       ('Keysi', (SELECT RoomID FROM Rooms WHERE RoomName='The Newest Room'), 1),
-       ('Joshua', (SELECT RoomID FROM Rooms WHERE RoomName='The Oldest'), 1),
-       ('Timmy', (SELECT RoomID FROM Rooms WHERE RoomName='Middle Room'), 1),
-       ('Rini', NULL, 0),
-       ('Salem', NULL, 0)
+INSERT INTO Users (Username, CurrentRoom, IsPlayer, IsHost)
+VALUES ('Jordan', (SELECT RoomID FROM Rooms WHERE RoomName='The Newest Room'), 1, 0),
+       ('Keysi', (SELECT RoomID FROM Rooms WHERE RoomName='The Newest Room'), 1, 1),
+       ('Joshua', (SELECT RoomID FROM Rooms WHERE RoomName='The Oldest'), 1, 1),
+       ('Timmy', (SELECT RoomID FROM Rooms WHERE RoomName='Middle Room'), 1, 1),
+       ('Rini', NULL, 0, 0),
+       ('Salem', NULL, 0, 0)
 
 SELECT * FROM Users
 SELECT * FROM Rooms ORDER BY CreationDate DESC
