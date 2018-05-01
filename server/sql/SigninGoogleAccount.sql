b25c29f10b300eb074dee9953ba9a5a43778ae63
Jordan Barnes <rjordanbarnes@gmail.com>
2018-05-01 14:07:53 -0700

    Avatar now updates upon sign in.

--- server/sql/SigninGoogleAccount.sql
+++ server/sql/SigninGoogleAccount.sql
@@ -2,9 +2,11 @@
 --DECLARE @avatarURL VARCHAR(2083) = ;
 --DECLARE @googleUsername NVARCHAR(25) = ;
 
--- If GoogleID isn't already in the Account table, create new account.
+-- If GoogleID isn't already in the Account table, create new account. Otherwise update the avatar.
 IF ((SELECT COUNT(*) FROM Account WHERE GoogleID = @googleID) = 0)
   INSERT INTO Account(GoogleID, Username, AvatarURL) VALUES (@googleID, @googleUsername, @avatarURL)
+ELSE
+  UPDATE Account SET AvatarURL = @avatarURL WHERE GoogleID = @googleID
 
 -- Return Account info
 SELECT AccountSK AS accountSK,
